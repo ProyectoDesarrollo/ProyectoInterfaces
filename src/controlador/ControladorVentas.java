4faa67d54f0361f908b863929e5375b87bbd1e3c
usuario <usuario@192.168.1.39>
2015-11-19 18:30:27 +0100

    18:30_19/11/2015

---
+++ src/controlador/ControladorVentas.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import vista.Interface;
+
+/**
+ *
+ * @author usuario
+ */
+public class ControladorVentas implements ActionListener,MouseListener{
+
+    Interface vista;
+    
+    public enum AccionMVC{
+        btnAñadir_Pedido,
+        btnModificar_Pedido,
+        btnEliminar_Pedido,
+        btnBuscar_Pedido
+    }
+    
+    public ControladorVentas(Interface vista){
+        this.vista=vista;
+    }
+    
+    public void iniciar(){
+         
+    }
+    
+    public void actionPerformed(ActionEvent e) {}
+
+    public void mouseClicked(MouseEvent e) {}
+
+    public void mousePressed(MouseEvent e) {}
+
+    public void mouseReleased(MouseEvent e) {}
+
+    public void mouseEntered(MouseEvent e) {}
+
+    public void mouseExited(MouseEvent e) {}
+    
+}
