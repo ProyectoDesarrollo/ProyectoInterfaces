6e9f2a648cb3f7f49cc7e940cbd6b5135855b941
usuario <usuario@192.168.1.39>
2015-11-22 18:17:42 +0100

    18:17_22/11/2015

---
+++ src/modelo/DatabaseSQLite.java
@@ -0,0 +1,30 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author usuario
+ */
+public class DatabaseSQLite {
+    
+    Connection conexion;
+    
+    public DatabaseSQLite(){
+        
+        try{
+            Class.forName("org.sqlite.JDBC");
+            conexion=DriverManager.getConnection("jdbc:sqlite:DatabaseLetras.db3");
+       }catch(SQLException ex){
+            System.err.println("No se ha conectado"+ex.getMessage());
+       }catch(ClassNotFoundException e){
+            System.err.println("No se encuentra la clase" +e.getMessage());
+       }
+    }
+        public Connection getConexion(){
+            return this.conexion;
+        }
+   }   
+
--- src/modelo/ModeloVentas.java
+++ src/modelo/ModeloVentas.java
@@ -1,20 +1,74 @@
 package modelo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-public class ModeloVentas {
+public class ModeloVentas extends Database{
     
-    private Database db = null;
+    //private Database db = null;
     
     public ModeloVentas() {
         
-        db = new Database();
+        //db = new Database();
         
     }
     
+    public DefaultTableModel getTablaArticulos(){
+        
+        DefaultTableModel tablemodel = new DefaultTableModel();
+        int registros=0;
+        String[] columNames = {"ID", "Nombre", "Stock", "Precio"};
+        
+        try{
+            
+            PreparedStatement pstm = this.getConnection().prepareStatement( "SELECT count(*) as Total FROM Articulos");
+            ResultSet res = pstm.executeQuery();
+            res.next();
+            registros=res.getInt("total");
+            res.close();
+            
+            
+        }catch(SQLException e){            
+            System.err.println( e.getMessage() );
+        }
+        
+        //se crea una matriz con tantas filas y columnas que necesite(de clase object para que no haya problemas)
+        Object[][] data = new String[registros][4];
+        
+        try{
+            //realizamos la consulta sql 
+            PreparedStatement pstm = this.getConnection().prepareStatement("SELECT * FROM Articulos");
+            ResultSet res = pstm.executeQuery();
+            int i=0;
+            
+            while(res.next()){ //y llenamos los datos en la matriz
+                data[i][0] = res.getString("ID");
+                data[i][1] = res.getString("Nombre");
+                data[i][2] = res.getString("Stock");
+                data[i][3] = res.getString("Precio");
+                i++;
+            }
+            
+            res.close();
+            //se añade la matriz de datos en el DefaultTableModel
+            tablemodel.setDataVector(data, columNames );            
+            
+        }catch(SQLException e){
+                
+            System.err.println( e.getMessage() );
+            JOptionPane.showMessageDialog(null, "No se ha podido conectar con la base de datos.");
+        }catch(Exception e){
+            
+            JOptionPane.showMessageDialog(null, "Ha ocurrido un error.");
+        }
+        return tablemodel;
+    }
+    /*
     public DefaultTableModel getTablaArticulos() {
         
         DefaultTableModel tableModel = new DefaultTableModel () {
@@ -49,7 +103,7 @@ public class ModeloVentas {
             
         }
         
-        Object[][] data = new String[registros][7];
+        Object[][] data = new String[registros][4];
         
         // Se rellenara en una matriz el contenido por celdas de la tabla.
         try {
@@ -106,5 +160,5 @@ public class ModeloVentas {
         return r;
         
     }
-    
+    */
 }
